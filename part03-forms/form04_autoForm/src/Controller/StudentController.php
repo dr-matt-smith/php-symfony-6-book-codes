d37291cf90ca554dec1beb31680cc41765d28c8d
dr-matt-smith <matt.smith@itb.ie>
2022-03-15 16:13:10 +0000

    codes for Forms section completed

---
+++ part03-forms/form04_autoForm/src/Controller/StudentController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use App\Repository\StudentRepository;
+
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Entity\Student;
+
+class StudentController extends AbstractController
+{
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/student/new', name: 'student_new_form', methods: ["POST", "GET"])]
+    public function new(Request $request): Response
+    {
+        // create a task and give it some dummy data for this example
+        $student = new Student();
+
+        // create a form with 'firstName' and 'surname' text fields
+        $form = $this->createFormBuilder($student)
+            ->add('firstName', TextType::class)
+            ->add('surname', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Student'))->getForm();
+
+        // if was POST submission, extract data and put into '$student'
+        $form->handleRequest($request);
+
+        // if SUBMITTED & VALID - go ahead and create new object
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->create($student);
+        }
+
+        // render the form for the user
+        $template = 'student/new.html.twig';
+        $argsArray = [
+            'form' => $form->createView(),
+        ];
+
+        return $this->render($template, $argsArray);
+    }
+
+
+
+    #[Route('/student/processNewForm', name: 'student_process_new_form')]
+    public function processNewForm(Request $request): Response
+    {
+        // extract name values from POST data
+        $firstName = $request->request->get('firstName');
+        $surname = $request->request->get('surname');
+
+        // valid if neither value is EMPTY
+        $isValid = !empty($firstName) && !empty($surname);
+        if(!$isValid){
+            $this->addFlash(
+                'error',
+                'student firstName/surname cannot be an empty string'
+            );
+
+            return $this->newForm();
+        }
+
+        // forward this to the createAction() method
+        return $this->create($firstName, $surname);
+    }
+
+    #[Route('/student', name: 'student_list')]
+    public function list(ManagerRegistry $doctrine): Response
+    {
+        $studentRepository = $doctrine->getRepository(Student::class);
+        $students = $studentRepository->findAll();
+
+        $template = 'student/list.html.twig';
+        $args = [
+            'students' => $students
+        ];
+        return $this->render($template, $args);
+    }
+
+    #[Route('/student/{id}', name: 'student_show')]
+    public function show(Student $student): Response
+    {
+        $template = 'student/show.html.twig';
+        $args = [
+            'student' => $student
+        ];
+
+        if (!$student) {
+            $template = 'error/404.html.twig';
+        }
+        return $this->render($template, $args);
+    }
+
+
+    public function create(Student $student): Response
+    {
+        $em = $this->doctrine->getManager();
+        $em->persist($student);
+        $em->flush();
+
+        return $this->redirectToRoute('student_list');
+    }
+
+    #[Route('/student/delete/{id}', name: 'student_delete')]
+    public function delete(Student $student, ManagerRegistry $doctrine)
+    {
+        // store ID so can report it later
+        $id = $student->getId();
+
+        // tells Doctrine you want to (eventually) delete the Student (no queries yet)
+        $em = $doctrine->getManager();
+        $em->remove($student);
+
+        // actually executes the queries (i.e. the DELETE query)
+        $em->flush();
+
+        return new Response('Deleted student with id '.$id);
+    }
+
+    #[Route('/student/update/{id}/{newFirstName}/{newSurname}', name: 'student_update')]
+    public function update(Student $student, string $newFirstName, string $newSurname, ManagerRegistry $doctrine)
+    {
+        $student->setFirstName($newFirstName);
+        $student->setSurname($newSurname);
+
+        $em = $doctrine->getManager();
+        $em->flush();
+
+        return $this->redirectToRoute('student_show', [
+            'id' => $student->getId()
+        ]);
+    }
+
+}
---
+++ part03-forms/form07_customFormDisplay/src/Form/StudentType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('surname')
+            ->add('save', SubmitType::class, array('label' => 'Create Student'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
